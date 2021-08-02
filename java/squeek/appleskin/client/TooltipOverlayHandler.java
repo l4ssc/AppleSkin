d5cc543d8a31c17739fcf422f6a9addbbe366fcc
lassc <lassc.alex@protonmail.com>
2021-08-02 03:17:26 -0500

    Change tooltips to show hearts instead of hunger/saturation

--- java/squeek/appleskin/client/TooltipOverlayHandler.java
+++ java/squeek/appleskin/client/TooltipOverlayHandler.java
@@ -81,7 +81,6 @@ public class TooltipOverlayHandler
 			return;
 
 		int biggestHunger = Math.max(defaultFoodValues.hunger, modifiedFoodValues.hunger);
-		float biggestSaturationIncrement = Math.max(defaultFoodValues.getSaturationIncrement(), modifiedFoodValues.getSaturationIncrement());
 
 		int barsNeeded = (int) Math.ceil(Math.abs(biggestHunger) / 2f);
 		boolean hungerOverflow = barsNeeded > 10;
@@ -89,29 +88,23 @@ public class TooltipOverlayHandler
 		if (hungerOverflow)
 			barsNeeded = 1;
 
-		int saturationBarsNeeded = (int) Math.max(1, Math.ceil(Math.abs(biggestSaturationIncrement) / 2f));
-		boolean saturationOverflow = saturationBarsNeeded > 10;
-		String saturationText = saturationOverflow ? ((biggestSaturationIncrement < 0 ? -1 : 1) * saturationBarsNeeded) + "x " : null;
-		if (saturationOverflow)
-			saturationBarsNeeded = 1;
-
 		int toolTipBottomY = toolTipY + toolTipH + 1 + TOOLTIP_REAL_HEIGHT_OFFSET_BOTTOM;
 		int toolTipRightX = toolTipX + toolTipW + 1 + TOOLTIP_REAL_WIDTH_OFFSET_RIGHT;
 
 		boolean shouldDrawBelow = toolTipBottomY + 20 < scale.getScaledHeight() - 3;
 
 		int rightX = toolTipRightX - 3;
-		int leftX = rightX - (Math.max(barsNeeded * 9 + (int) (mc.fontRenderer.getStringWidth(hungerText) * 0.75f), saturationBarsNeeded * 6 + (int) (mc.fontRenderer.getStringWidth(saturationText) * 0.75f))) - 3;
+		int leftX = rightX - barsNeeded * 9 + (int) (mc.fontRenderer.getStringWidth(hungerText) * 0.75f - 3);
 		int topY = (shouldDrawBelow ? toolTipBottomY : toolTipY - 20 + TOOLTIP_REAL_HEIGHT_OFFSET_TOP);
-		int bottomY = topY + 19;
+		int bottomY = topY + 11;
 
 		GlStateManager.disableLighting();
 		GlStateManager.disableDepth();
 
 		// bg
-		Gui.drawRect(leftX - 1, topY, rightX + 1, bottomY, 0xF0100010);
-		Gui.drawRect(leftX, (shouldDrawBelow ? bottomY : topY - 1), rightX, (shouldDrawBelow ? bottomY + 1 : topY), 0xF0100010);
-		Gui.drawRect(leftX, topY, rightX, bottomY, 0x66FFFFFF);
+		Gui.drawRect(leftX - 2, topY, rightX + 1, bottomY, 0xF0100010);
+		Gui.drawRect(leftX - 1, (shouldDrawBelow ? bottomY : topY - 1), rightX, (shouldDrawBelow ? bottomY + 1 : topY), 0xF0100010);
+		Gui.drawRect(leftX - 1, topY, rightX, bottomY, 0x66FFFFFF);
 
 		// drawRect disables blending and modifies color, so reset them
 		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
@@ -119,32 +112,19 @@ public class TooltipOverlayHandler
 		GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
 		int x = rightX - 2;
-		int startX = x;
-		int y = bottomY - 18;
+		int y = bottomY - 10;
 
-		mc.getTextureManager().bindTexture(Gui.ICONS);
+		mc.getTextureManager().bindTexture(modIcons);
 
 		for (int i = 0; i < barsNeeded * 2; i += 2)
 		{
 			x -= 9;
 
-			if (modifiedFoodValues.hunger < 0)
-				gui.drawTexturedModalRect(x, y, 34, 27, 9, 9);
-			else if (modifiedFoodValues.hunger > defaultFoodValues.hunger && defaultFoodValues.hunger <= i)
-				gui.drawTexturedModalRect(x, y, 133, 27, 9, 9);
-			else if (modifiedFoodValues.hunger > i + 1 || defaultFoodValues.hunger == modifiedFoodValues.hunger)
-				gui.drawTexturedModalRect(x, y, 16, 27, 9, 9);
-			else if (modifiedFoodValues.hunger == i + 1)
-				gui.drawTexturedModalRect(x, y, 124, 27, 9, 9);
-			else
-				gui.drawTexturedModalRect(x, y, 34, 27, 9, 9);
-
-			GlStateManager.color(1.0F, 1.0F, 1.0F, .25F);
-			gui.drawTexturedModalRect(x, y, defaultFoodValues.hunger - 1 == i ? 115 : 106, 27, 9, 9);
+			gui.drawTexturedModalRect(x, y, 36, 0, 9, 9);
 			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 
 			if (modifiedFoodValues.hunger > i)
-				gui.drawTexturedModalRect(x, y, modifiedFoodValues.hunger - 1 == i ? 61 : 52, 27, 9, 9);
+				gui.drawTexturedModalRect(x, y, modifiedFoodValues.hunger - 1 == i ? 54 : 45, 0, 9, 9);
 		}
 		if (hungerText != null)
 		{
@@ -154,37 +134,6 @@ public class TooltipOverlayHandler
 			GlStateManager.popMatrix();
 		}
 
-		y += 10;
-		x = startX;
-		float modifiedSaturationIncrement = modifiedFoodValues.getSaturationIncrement();
-		float absModifiedSaturationIncrement = Math.abs(modifiedSaturationIncrement);
-
-		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-		mc.getTextureManager().bindTexture(modIcons);
-		for (int i = 0; i < saturationBarsNeeded * 2; i += 2)
-		{
-			float effectiveSaturationOfBar = (absModifiedSaturationIncrement - i) / 2f;
-
-			x -= 6;
-
-			boolean shouldBeFaded = absModifiedSaturationIncrement <= i;
-			if (shouldBeFaded)
-				GlStateManager.color(1.0F, 1.0F, 1.0F, .5F);
-
-			gui.drawTexturedModalRect(x, y, effectiveSaturationOfBar >= 1 ? 21 : effectiveSaturationOfBar > 0.5 ? 14 : effectiveSaturationOfBar > 0.25 ? 7 : effectiveSaturationOfBar > 0 ? 0 : 28, modifiedSaturationIncrement >= 0 ? 27 : 34, 7, 7);
-
-			if (shouldBeFaded)
-				GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-		}
-		if (saturationText != null)
-		{
-			GlStateManager.pushMatrix();
-			GlStateManager.scale(0.75F, 0.75F, 0.75F);
-			mc.fontRenderer.drawStringWithShadow(saturationText, x * 4 / 3 - mc.fontRenderer.getStringWidth(saturationText) + 2, y * 4 / 3 + 1, 0xFFDDDDDD);
-			GlStateManager.popMatrix();
-		}
-
-		GlStateManager.disableBlend();
 		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 
 		// reset to drawHoveringText state
